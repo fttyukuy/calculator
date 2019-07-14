905ec552e05c1f24a3575b9729584912b15d180d
fttyukuy <47567266+fttyukuy@users.noreply.github.com>
2019-07-14 10:53:41 +0800

    基本布局完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,16 @@
 
 Page({
   data: {
-   
+    button_row1: ["c", "÷", "×","Bk"],
+    button_row2: [7, 8, 9, "-"],
+    button_row3: [4, 5, 6, "+"],
+    button_row4: [1, 2, 3,''],
+    button_row5: ["%", 0, ".",''],
+    input: '',
+    result: ''
   },
-  onLoad: function () {}
+  onLoad: function () {},
+  handleTap(event) {
+    console.log(event.currentTarget.dataset.value);
+  }
 })
