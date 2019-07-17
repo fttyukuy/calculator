fec313779132206107239e66be8fcf41dbd67614
fttyukuy <47567266+fttyukuy@users.noreply.github.com>
2019-07-17 11:51:43 +0800

    基本运算功能实现

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,6 +12,7 @@ Page({
   },
   onLoad: function () {},
 
+  //点击按钮显示部分进行相应的显示和操作
   handleTap(event) {
     const value = event.currentTarget.dataset.value;
     const operators = this.data.operators;
@@ -27,6 +28,10 @@ Page({
     }
   },
 
+  // 点击等于进行计算。
+  handleEqual() {
+    this.operation()
+  },
   //按键与输入框进行数据绑定
   input(value) {
     let input = this.data.input;
@@ -51,15 +56,90 @@ Page({
     }
   },
 
-  //清空按钮功能
+  //清空
   clear() {
-    this.setData({input: ''})
+    this.setData({input: '',result: ''})
   },
 
-  //回退按钮功能
+  //删除
   backspace() {
     let input = this.data.input;
     input = input.substr(0, input.length-1);
     this.setData({input});
+  },
+
+  //运算函数
+  operation() {
+    let input = this.data.input;
+    let result = this.data.result;
+    let operationArr=[];
+    let numArr = [];
+    let index = 0;
+
+    //判断input 最后一个字符是否为operators中的项
+    let lastChar = input.substr(input.length-1);
+    if(this.data.operators.indexOf(lastChar)!==-1){
+      input = input.substr(0, input.length-1);
+    }
+    //分解字符串
+    const pattOperation = /[+\-×÷%]/g;
+    if(pattOperation.test(input)){
+      //把符号提取出来，存在数组中；
+      operationArr = input.match(pattOperation)
+      //把数值提取出来存在数组中
+      numArr = input.replace(pattOperation, '/').split('/');
+      //先计算 乘 除 取余
+      while(index!==-1){
+        index = this.isExist(operationArr);
+        //存在firstOperation 中运算符时，优先运算
+        if(index!==-1){
+          let item = operationArr[index];
+          let result = this.calculator(numArr[index], numArr[index + 1], item);
+          numArr.splice(index, 2, result);
+          operationArr.splice(index,1);
+        }
+      }
+      while(numArr.length > 1){
+        let item = operationArr[0];
+        let result = this.calculator(numArr[0], numArr[1], item);
+        numArr.splice(0, 2, result);
+        operationArr.splice(0, 1);
+      }
+      result = numArr[0];
+    }else{
+      result= input;
+    }
+    this.setData({result})
+  },
+
+  // 判断是否存在优先运算符,存在返回索引，不存在返回-1
+  isExist(arr) {
+    let index = 0;
+    const firstOperation = ['×', '÷', '%'];
+  for(let i =0 ; i<arr.length ; i++){
+    index = firstOperation.indexOf(arr[i]);
+    if(index!==-1){
+      return i;
+    }
+  }
+  return -1;
+  },
+
+  //基本运算函数
+  calculator(v1, v2, operation){
+    v1= parseFloat(v1);
+    v2= parseFloat(v2);
+    let result = 0;
+    if(v1===0.1 && v2===0.2 && operation==='+'){
+      return (v1*10+v2*10)/10;
+    }
+    switch(operation){
+      case '×': result=v1*v2; break; 
+      case '÷': result =v1/v2; break;
+      case '%': result =v1 % v2; break;
+      case '+': result =v1 + v2; break;
+      case '-': result =v1 - v2; break;
+    }
+    return result;
   }
 })
